4d25b8dae482691fc9a307fa4691f67dd7d710b0
joshwilh <joshwilh@umich.edu>
2017-10-14 13:27:13 -0400

    Initial Commit

---
+++ Visibility Graph Path Planning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Visibility Graph Path Planning
+//
+//  Created by Joshua Wilhelm on 10/14/17.
+//  Copyright © 2017 Joshua Wilhelm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
